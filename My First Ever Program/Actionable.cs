67fba7e80b019c4eec115d0c76c7e3662a52455b
Thomas Micol <thomasmicol@gmail.com>
2018-03-16 00:15:32 +1300

    Static class that holds logic for the actions defined in the interaction xml. This can be added to indefintelly as more and more actions are added to the xml. This was done with the ease of adding more interactables without taking into account for scalabilty and the bad smells that will arise from this soon to be massive class

---
+++ My First Ever Program/Actionable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Ever_Program
+{
+    class Actionable
+    {
+
+        public static ResultPackage Investigate(object aPlayer)
+        {
+            Console.WriteLine("You look and see nothing");
+            return new ResultPackage();
+        }
+
+        public static void Leave(Player aPlayer)
+        {
+            aPlayer.RollNewInteractable();
+        }
+    }
+}
